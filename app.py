f7efdb0864b44c8e198228cfba8b8733c4cf3764
goktug-aygun <goktug.aygun@sabanciuniv.edu>
2024-12-01 21:32:06 +0300

    Integrated .env file structure Modified deprecated use of datetime Filter results based on similarity Added the functionality to continue with a given chat or start a new one Minor Changes

--- app.py
+++ app.py
@@ -1,18 +1,40 @@
-from flask import Flask, request, jsonify
-import time
-from datetime import datetime
-from langchain_chroma import Chroma
-from langchain_ollama import OllamaEmbeddings
-from langchain_ollama.llms import OllamaLLM
 from langchain.prompts import ChatPromptTemplate
+from langchain_ollama import OllamaEmbeddings
 from pymongo.mongo_client import MongoClient
+from langchain_ollama.llms import OllamaLLM
+from flask import Flask, request, jsonify
 from pymongo.server_api import ServerApi
+from datetime import datetime, timezone
+from langchain_chroma import Chroma
+from dotenv import load_dotenv
+from bson import ObjectId
+import time
+import os
 
 app = Flask(__name__)
 
-uri = "mongodb+srv://admin:mfB7ViDCuIiBSaeQ@courseassistantdb.j70fx.mongodb.net/?retryWrites=true&w=majority&appName=CourseAssistantDB"
+# Get info from .env file
+load_dotenv()
+
+# Mongo Related Variables
+USERNAME = os.getenv("DB_USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+CLUSTER_ADDRESS = os.getenv("CLUSTER_ADDRESS")
+APP_NAME = os.getenv("APP_NAME")
+
+# LLM Related Variables
+MODEL = os.getenv("MODEL")
+
+
+# RAG Related Variables
+CHROMA_PATH = os.getenv("CHROMA_PATH")
+THRESHOLD = 0.3
+
+uri = f"mongodb+srv://{USERNAME}:{PASSWORD}@{CLUSTER_ADDRESS}/?retryWrites=true&w=majority&appName={APP_NAME}"
+
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
+
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
@@ -25,11 +47,9 @@ course_db = client.CourseAssistantDB
 chats_collection = course_db.Chats
 
 
-MODEL="llama3.1:8b"
 embedding_function = OllamaEmbeddings(model=MODEL)
 model = OllamaLLM(model=MODEL)
 
-CHROMA_PATH = "chroma"
 
 PROMPT_TEMPLATE = """
 Answer the question based only on the following context:
@@ -39,34 +59,43 @@ Answer the question based only on the following context:
 ---
 
 Answer the question based on the above context: {question}
+
+If no context is provided, kindly inform the user that you cannot answer the question since it is not related with school-related work.
 """
 #http://localhost:5000/course/query
 @app.route('/course/query', methods=['POST'])
 def query():
-    course = request.json.get('course')
+    chat_course = request.json.get('course')
     query_text = request.json.get('query_text')
     user_id = request.json.get('user_id')
+    chat_id = request.json.get('chat_id')
+    
 
-    if not query_text or not course or not user_id:
+    if not query_text or not chat_course or not user_id:
         return jsonify({"error": "Missing query text, course, or user_id"}), 400
     
-    chroma_path = f"{CHROMA_PATH}/{course}"
-    print(chroma_path)
+
+    chroma_path = f"{CHROMA_PATH}/{chat_course}"
+    # print(chroma_path)
     start_time = time.time()
     
     db = Chroma(persist_directory=chroma_path, embedding_function=embedding_function)
     results = db.similarity_search_with_relevance_scores(query_text)
-    if len(results) == 0:
+
+    filtered_results = [ (doc, score) for doc, score in results if score >= THRESHOLD ]
+
+    if len(filtered_results) == 0:
         return jsonify({"error": "Unable to find matching results"}), 404
     
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
 
+  
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    print("here1")
+
     response_text = model.invoke(prompt)
-    print("here2")
-    sources = [doc.metadata.get("source", None) for doc, _score in results]
+
+    sources = list(set(doc.metadata.get("source", None) for doc, _score in results))
 
     end_time = time.time()
 
@@ -74,38 +103,38 @@ def query():
     user_message = {
         "sender": "user",
         "message_content": query_text,
-        "timestamp": datetime.utcnow().isoformat()
+        "timestamp": datetime.now(timezone.utc).isoformat()
     }
     chatbot_message = {
         "sender": "chatbot",
         "message_content": response_text,
-        "timestamp": datetime.utcnow().isoformat()
+        "timestamp": datetime.now(timezone.utc).isoformat()
     }
-    print("here3")
+    # print("here3")
 
-    # Check if an entry already exists for the user
-    existing_chat = chats_collection.find_one({"user_id": user_id})
-    print("here4")
 
-    if existing_chat:
-        # Append to the existing messages array and update the last_message_time
-        chats_collection.update_one(
-            {"user_id": user_id},
-            {
-                "$push": {"messages": {"$each": [user_message, chatbot_message]}},
-                "$set": {"last_message_time": datetime.utcnow().isoformat()}
-            }
-        )
-    else:
-        # Create a new chat entry
+    if (chat_id):   # the chat has been previously created, append to chat history        
+        existing_chat = chats_collection.find_one({"_id": ObjectId(f"{chat_id}")})
+        if (existing_chat): # prevents problems if random chat id is given somehow
+            chats_collection.update_one(
+                {"user_id": user_id},
+                {
+                    "$push": {"messages": {"$each": [user_message, chatbot_message]}},
+                    "$set": {"last_message_time": datetime.now(timezone.utc).isoformat()}
+                }
+            )
+        else:
+            return jsonify({"error": "Bad chat history ID"}), 400 
+    else:   # Create a new chat entry
         chat_entry = {
             "user_id": user_id,
-            "started_at": datetime.utcnow().isoformat(),
-            "last_message_time": datetime.utcnow().isoformat(),
+            "started_at": datetime.now(timezone.utc).isoformat(),
+            "last_message_time": datetime.now(timezone.utc).isoformat(),
             "messages": [user_message, chatbot_message]
         }
         chats_collection.insert_one(chat_entry)
 
+    
     return jsonify({
         "response": response_text,
         "sources": sources,
@@ -114,4 +143,3 @@ def query():
 
 if __name__ == "__main__":
     app.run()
-    
