f20677796d6a4315535edd241776b3d2bd935703
goktug-aygun <goktug.aygun@sabanciuniv.edu>
2024-12-22 17:58:37 +0300

    Working App

--- Routes/query.py
+++ Routes/query.py
@@ -1,3 +1,118 @@
+# from langchain.prompts import ChatPromptTemplate
+# from langchain_ollama import OllamaEmbeddings
+# from langchain_ollama.llms import OllamaLLM
+# from datetime import datetime, timezone
+# from langchain_chroma import Chroma
+# from flask import request, jsonify
+# from dotenv import load_dotenv
+# from bson import ObjectId
+# import time
+# import os
+
+# load_dotenv()
+
+# MODEL = os.getenv("MODEL")
+# CHROMA_PATH = os.getenv("CHROMA_PATH")
+# THRESHOLD = 0.3
+
+# embedding_function = OllamaEmbeddings(model=MODEL)
+# model = OllamaLLM(model=MODEL)
+
+# PROMPT_TEMPLATE = """
+# Answer the question based only on the following context:
+
+# {context}
+
+# ---
+
+# Answer the question based on the above context: {question}
+
+# """
+
+
+# def query(course_db, term, course, chat_id=None):
+    
+#     # These will be obtained from raw JSON body
+#     query_text = request.json.get('query_text')
+#     user_id = request.json.get('user_id')
+
+#     if not query_text or not course or not user_id:
+#         return jsonify({"error": "Missing query text, course, or user_id"}), 400
+    
+#     chroma_path = f"{CHROMA_PATH}/{term}/{course}/"
+
+#     start_time = time.time()
+
+#     db = Chroma(persist_directory=chroma_path, embedding_function=embedding_function)
+#     results = db.similarity_search_with_relevance_scores(query_text)
+
+#     filtered_results = [(doc, score) for doc, score in results if score >= THRESHOLD]
+    
+#     if len(filtered_results) == 0:
+#         end_time = time.time()
+#         return jsonify({
+#             "response": "Unable to find matching results from course material.",
+#             "sources": [],
+#             "execution_time": f"{end_time - start_time:.2f} seconds"
+#         })
+
+#     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+
+#     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+#     prompt = prompt_template.format(context=context_text, question=query_text)
+
+#     response_text = model.invoke(prompt)
+
+#     sources = list(set((doc.metadata.get("source", None)) for doc, _score in filtered_results))
+
+#     end_time = time.time()
+
+#     # Construct messages
+#     user_message = {
+#         "sender": "user",
+#         "message_content": query_text,
+#         "timestamp": datetime.now(timezone.utc).isoformat()
+#     }
+#     chatbot_message = {
+#         "sender": "chatbot",
+#         "message_content": response_text,
+#         "timestamp": datetime.now(timezone.utc).isoformat()
+#     }
+
+#     if chat_id:  # the chat has been previously created, append to chat history
+#         course_db.Chats.update_one(
+#             {"_id": ObjectId(f"{chat_id}")},
+#             {
+#                 "$push": {"messages": {"$each": [user_message, chatbot_message]}},
+#                 "$set": {"last_message_time": datetime.now(timezone.utc).isoformat()}
+#             }
+#         )
+#     else:  # Create a new chat entry
+#         # Create a short title for this new conversation
+#         title_prompt = ChatPromptTemplate.from_template(
+#             "Create a concise title (up to 6 words) for this conversation based on the user query:\n\nUser Query: {question}"
+#         )
+#         title_input = title_prompt.format(question=query_text)
+#         title_response = model.invoke(title_input)
+
+#         title = title_response.strip().replace('"', '').replace("'", '')
+
+#         chat_entry = {
+#             "user_id": user_id,
+#             "started_at": datetime.now(timezone.utc).isoformat(),
+#             "last_message_time": datetime.now(timezone.utc).isoformat(),
+#             "messages": [user_message, chatbot_message],
+#             "title": f"{title}"
+#         }
+#         course_db.Chats.insert_one(chat_entry)
+
+#     return jsonify({
+#         "response": response_text,
+#         "sources": sources,
+#         "execution_time": f"{end_time - start_time:.2f} seconds"
+#     })
+
+
 from langchain.prompts import ChatPromptTemplate
 from langchain_ollama import OllamaEmbeddings
 from langchain_ollama.llms import OllamaLLM
@@ -30,11 +145,23 @@ Answer the question based on the above context: {question}
 """
 
 
-def query(course_db, term, course, chat_id=None):
-    
+def query(course_db):
     # These will be obtained from raw JSON body
+    course = request.json.get('course')
+    term = request.json.get('term') or None
+
+
     query_text = request.json.get('query_text')
     user_id = request.json.get('user_id')
+    chat_id = request.json.get('chat_id')
+
+    print("query icinde")
+    print(f"term: {term}")
+    print(f"course: {course}")
+    print(f"query text: {query_text}")
+    print(f"user id: {user_id}")
+    print(f"chat id: {chat_id}")
+
 
     if not query_text or not course or not user_id:
         return jsonify({"error": "Missing query text, course, or user_id"}), 400
@@ -48,6 +175,7 @@ def query(course_db, term, course, chat_id=None):
 
     filtered_results = [(doc, score) for doc, score in results if score >= THRESHOLD]
     
+    # Burayi degistir kaynak yoksa ona gore mesaj at 200 return atma
     if len(filtered_results) == 0:
         end_time = time.time()
         return jsonify({
@@ -63,7 +191,12 @@ def query(course_db, term, course, chat_id=None):
 
     response_text = model.invoke(prompt)
 
-    sources = list(set((doc.metadata.get("source", None)) for doc, _score in filtered_results))
+    sources = []
+    for doc, _score in filtered_results:
+        sources.append({
+            "source": doc.metadata.get("source"),
+            "page_number": doc.metadata.get("page_number")
+        })
 
     end_time = time.time()
 
@@ -97,17 +230,21 @@ def query(course_db, term, course, chat_id=None):
 
         title = title_response.strip().replace('"', '').replace("'", '')
 
+
         chat_entry = {
             "user_id": user_id,
             "started_at": datetime.now(timezone.utc).isoformat(),
             "last_message_time": datetime.now(timezone.utc).isoformat(),
             "messages": [user_message, chatbot_message],
-            "title": f"{title}"
+            "title": title
         }
-        course_db.Chats.insert_one(chat_entry)
+
+        result = course_db.Chats.insert_one(chat_entry) # result holds the information about newly inserted chat
+        chat_id = str(result.inserted_id)
 
     return jsonify({
-        "response": response_text,
+        "chat_id": chat_id,
+        "model_response": response_text,
         "sources": sources,
         "execution_time": f"{end_time - start_time:.2f} seconds"
-    })
+    }), 200
--- vector_database.py
+++ vector_database.py
@@ -1,3 +1,74 @@
+# from langchain_community.document_loaders import DirectoryLoader
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.schema import Document
+# from langchain_ollama import OllamaEmbeddings
+# from langchain_community.vectorstores import Chroma
+# import shutil
+# import time
+# import os
+
+# MODEL="llama3.1:8b"
+# CHROMA_PATH = "chroma"
+# DATA_PATH = "data"
+
+
+# def create_vector_database(term, course_code):
+#     start_time = time.time()
+#     generate_data_store(term, course_code)
+#     end_time = time.time()
+#     print(f"Database creation time: {end_time - start_time:.2f} seconds")
+
+
+# def generate_data_store(term, course_code):
+#     documents = load_documents(term, course_code)
+#     if not documents:
+#         print(f"No documents found for {course_code}.")
+#         return
+#     chunks = split_text(documents)
+#     save_to_chroma(chunks, term, course_code)
+
+
+# def load_documents(term, course_code):
+#     loader = DirectoryLoader(f"{DATA_PATH}/{term}/{course_code}") #glob can be specified
+#     documents = loader.load()
+
+#     return documents
+
+
+# def split_text(documents: list[Document]):
+#     text_splitter = RecursiveCharacterTextSplitter(
+#         chunk_size=1500,
+#         chunk_overlap=150,#10%
+#         length_function=len,
+#         add_start_index=True,
+#     )
+#     chunks = text_splitter.split_documents(documents)
+#     print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+#     return chunks
+
+
+# def save_to_chroma(chunks: list[Document], term, course_code):
+#     # Clear out the database first.
+#     course_chroma_path = f"{CHROMA_PATH}/{term}/{course_code}"
+#     if os.path.exists(course_chroma_path):
+#         shutil.rmtree(course_chroma_path)
+
+#     # Create a new DB from the documents.
+#     db = Chroma.from_documents(
+#         chunks, OllamaEmbeddings(model=MODEL), persist_directory=course_chroma_path
+#     )
+#     print(f"Saved {len(chunks)} chunks to {course_chroma_path}.")
+
+# if __name__=="__main__":
+#     term = 'F24-25'
+#     course_codes = ['CS307']
+#     for course_code in course_codes:
+#         create_vector_database(term, course_code)
+#         print(f"Processed the documents for {course_code}.")
+
+
+
 from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
@@ -43,6 +114,8 @@ def split_text(documents: list[Document]):
         add_start_index=True,
     )
     chunks = text_splitter.split_documents(documents)
+    for i, chunk in enumerate(chunks):
+        chunk.metadata["page_number"] = i + 1
     print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
 
     return chunks
